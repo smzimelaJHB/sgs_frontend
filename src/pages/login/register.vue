694462bdfbff190776bfd078fcbe0ce55e80cc13
smzimela <smzimela@student.wethinkcode.co.za>
2022-06-29 12:48:22 +0200

    signup form added an image

--- src/pages/login/register.vue
+++ src/pages/login/register.vue
@@ -1,31 +1,36 @@
 <template lang="pug">
-.position
-  q-form.q-gutter-md(@submit.prevent="submit")
-    q-input(type="text",filled,v-model="form.name",label="Name" required)
-    q-input(type="email",filled,v-model="form.email",label="Email" required)
-    //- 1st password field
-    q-input(v-show="my_type",type="password",filled,v-model="form.password",label="Password" required)
-    q-input(v-show="!my_type",type="text",filled,v-model="form.password",label="Password")
-    //- 2nd password field
-    q-input(v-show="my_type",type="password",filled,v-model="form.password_confirmation",label="Repeat Password" required)
-    q-input(v-show="!my_type",type="text",filled,v-model="form.password_confirmation",label="Repeat Password")
+.container
+  .position
+    q-img(src="signup.png",spinner-color="white",style="display:flex;max-height: 300px; max-width: 300px")
+    q-form.q-gutter-md(@submit.prevent="submit")
+      q-input(type="text",filled,v-model="form.name",label="Name" required)
+      q-input(type="email",filled,v-model="form.email",label="Email" required)
+      //- 1st password field
+      q-input(v-show="my_type",type="password",filled,v-model="form.password",label="Password" required)
+      q-input(v-show="!my_type",type="text",filled,v-model="form.password",label="Password")
+      //- 2nd password field
+      q-input(v-show="my_type",type="password",filled,v-model="form.password_confirmation",label="Repeat Password" required)
+      q-input(v-show="!my_type",type="text",filled,v-model="form.password_confirmation",label="Repeat Password")
 
-    q-toggle(v-model="form.viewPass",@click="show_pass",label="show password" color="pink",,style="float:left;")
+      q-toggle(v-model="form.viewPass",@click="show_pass",label="show password" color="pink",,style="float:left;")
 
-    q-btn(color='blue',type="submit",label="Submit",style="float:right;")
+      q-btn(color='blue',type="submit",label="Submit",style="float:right;")
 
 
 </template>
 
 <style>
-.position {
+.container {
   display: flex;
   max-width: 100%;
   justify-content: center;
+}
+
+.position {
   margin-top: 5%;
 }
 form {
-  min-width: 50%;
+  min-width: 70%;
 }
 </style>
 
