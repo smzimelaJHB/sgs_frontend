93aea74c542ecddc1d20347b941ce3c67d56f340
smzimela <smzimela@student.wethinkcode.co.za>
2022-07-04 21:18:38 +0200

    getting cookie first csrf on the form

---
+++ dist/spa/assets/use-quasar.d5be3785.js
@@ -0,0 +1,1 @@
+import{i as a,aM as r}from"./index.55ab12b1.js";function u(){return a(r)}export{u};
--- dist/spa/assets/use-quasar.fbaafb10.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aM as r}from"./index.d14d3567.js";function u(){return a(r)}export{u};
