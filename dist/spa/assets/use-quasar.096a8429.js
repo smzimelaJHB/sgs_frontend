7cf351fbc24600bd325fcc8bbc20f4e993e66175
smzimela <smzimela@student.wethinkcode.co.za>
2022-07-04 10:40:01 +0200

    modify script

---
+++ dist/spa/assets/use-quasar.096a8429.js
@@ -0,0 +1,1 @@
+import{i as a,aM as r}from"./index.c9e959c2.js";function u(){return a(r)}export{u};
--- dist/spa/assets/use-quasar.745e66b0.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aM as r}from"./index.ba201228.js";function u(){return a(r)}export{u};
